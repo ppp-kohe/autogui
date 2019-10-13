0dbdfd0a5ec523cbc991a7a7a0695faf5bf827a1
Kohei SAKURAI <k_sakurai@acm.org>
2019-10-13 21:01:39 +0900

    switching to Java11 with module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,19 @@
+module org.autogui {
+    requires transitive java.datatransfer;
+    requires transitive java.desktop;
+    requires transitive java.prefs;
+
+    exports org.autogui;
+
+    exports org.autogui.base;
+    exports org.autogui.base.log;
+    exports org.autogui.base.mapping;
+    exports org.autogui.base.type;
+
+    exports org.autogui.swing;
+    exports org.autogui.swing.icons;
+    exports org.autogui.swing.log;
+    exports org.autogui.swing.mapping;
+    exports org.autogui.swing.table;
+    exports org.autogui.swing.util;
+}
